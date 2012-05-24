4435f7503bbd17b031b8385b66984f96d4adb178
Alessandro Franceschi <al@lab42.it>
2012-05-24 14:00:03 +0000

    More documentation

--- manifests/directive.pp
+++ manifests/directive.pp
@@ -1,3 +1,26 @@
+# = Define: sudo::directive
+#
+# This defines places a directive for the sudoers file
+# On old versions of sudo ( < 1.7.2 ) it places a line in 
+# /etc/sudoers (The Concat module is required for it)
+# On more recent versions it just places a file in /etc/sudoers.d
+#
+# == Parameters
+#
+# [*source*]
+#   Sets the value of source parameter for the sudo fragment
+#
+# [*template*]
+#   Sets the value of content parameter for the sudo fragment
+#   Note: This option is alternative to the source one
+#
+# [*ensure*]
+#   Define if the fragment should be present (default) or 'absent'
+#
+# [*order*]
+#   Sets the order of the fragment inside /etc/sudoers or /etc/sudoers.d
+#   Default 20
+#
 define sudo::directive (
   $ensure  = present,
   $content = '',
