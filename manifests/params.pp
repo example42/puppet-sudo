66934feaff1f79427d8714b14c6ba036b3729763
erik-smit <smite@zylon.net>
2013-02-03 15:05:09 +0100

    EL5 has Sudo 1.7.2, but the distro-provided /etc/sudoers does not contain #includedir or create /etc/sudoers.d

--- manifests/params.pp
+++ manifests/params.pp
@@ -32,7 +32,8 @@ class sudo::params {
       default => '/etc/sudoers.d',
     },
     /(?i:RedHat|Centos|Scientific)/ => $::operatingsystemrelease ? {
-      '4'  => false,
+      /^4/    => false,
+      /^5/    => false,
       default => '/etc/sudoers.d',
     },
     default => '/etc/sudoers.d',
