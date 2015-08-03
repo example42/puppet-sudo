ff25111d1a4673a1baad94c799656a5e9934376b
Martin Alfke <ma@example42.com>
2015-08-03 16:51:36 +0200

    add first rspec test
    - pin rspec to 3.1.x on ruby 1.8.x
    - ignore bundle paths
    - run on new travis infrastructure

---
+++ spec/classes/sudo_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper.rb"
+
+describe 'sudo' do
+
+  let(:title) { 'sudo' }
+  let(:node) { 'rspec.example42.com' }
+  let(:facts) { {
+      :ipaddress => '10.42.42.42',
+      :concat_basedir => '/dne',
+      :operatingsystemrelease => '6.6',
+      :operatingsystem => 'Debian',
+      :osfamily => 'Debian',
+      :lsbdistcodename => 'Jessie'
+  } }
+
+  it { should contain_class('sudo') }
+
+end
